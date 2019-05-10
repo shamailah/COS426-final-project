32747c936d605dc8af29fd6fe2e43f8377a76301
simiboba <simisola@princeton.edu>
2019-05-10 15:28:13 -0400

    Add and Objects
    These changes allow you to add and remove objects from the scene

--- js/renderer.js
+++ js/renderer.js
@@ -26,16 +26,15 @@ var jupiter = createPlanet("textures/jupiter.jpg", 2, new THREE.Vector3(5, 0, 0)
 var saturn = createPlanet("textures/saturn.jpg", 2, new THREE.Vector3(10, 0, 0));
 var uranus = createPlanet("textures/uranusmap.jpg", 2, new THREE.Vector3(15, 0, 0));
 var neptune = createPlanet("textures/neptune.jpg", 2, new THREE.Vector3(20, 0, 0));
-
 // Controls
 controls.enablePan = true;
 controls.enableZoom = true;
 controls.enableRotate = true;
 controls.enableKeys = true;
-controls.minDistance
-controls.maxDistance
-controls.minPolarAngle
-controls.maxPolarAngle
+// controls.minDistance
+// controls.maxDistance
+// controls.minPolarAngle
+// controls.maxPolarAngle
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -53,6 +52,71 @@ scene.add(jupiter);
 scene.add(saturn);
 scene.add(uranus);
 scene.add(neptune);
+console.log(scene);
+
+this.sceneDatGui = new dat.GUI(); // controls the meshes in the scene
+this.controlListDatGui = new dat.GUI(); // controls each of the meshes
+this.sceneObject = new SceneObject(scene);
+
+var planetNames = ['mercury', 'venus', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune', 'earth'];
+
+for (var i = 0; i < planetNames.length; i++) {
+  this.sceneObject[planetNames[i]] = true;
+  this.sceneObject.bodies[planetNames[i]] = new PlanetInfo();
+  var controller = this.sceneDatGui.add(this.sceneObject, planetNames[i]);
+  this.sceneObject.bodies[planetNames[i]].controller = controller;
+  this.sceneObject.bodies[planetNames[i]].mesh = window[planetNames[i]];
+}
+
+// handling the controller events
+this.sceneObject.bodies.mercury.controller.onChange(function(value) {
+  if (value) scene.add(this.object.bodies.mercury.mesh);
+  else scene.remove(this.object.bodies.mercury.mesh)
+});
+this.sceneObject.bodies.venus.controller.onChange(function(value) {
+  if (value) scene.add(this.object.bodies.venus.mesh);
+  else scene.remove(this.object.bodies.venus.mesh);
+});
+this.sceneObject.bodies.mars.controller.onChange(function(value) {
+  if (value) scene.add(this.object.bodies.mars.mesh);
+  else scene.remove(this.object.bodies.mars.mesh);
+});
+this.sceneObject.bodies.jupiter.controller.onChange(function(value) {
+  if (value) scene.add(this.object.bodies.jupiter.mesh);
+  else scene.remove(this.object.bodies.jupiter.mesh);
+});
+this.sceneObject.bodies.saturn.controller.onChange(function(value) {
+  if (value) scene.add(this.object.bodies.saturn.mesh);
+  else scene.remove(this.object.bodies.saturn.mesh);
+});
+this.sceneObject.bodies.uranus.controller.onChange(function(value) {
+  if (value) scene.add(this.object.bodies.uranus.mesh);
+  else scene.remove(this.object.bodies.uranus.mesh);
+});
+this.sceneObject.bodies.neptune.controller.onChange(function(value) {
+  if (value) scene.add(this.object.bodies.neptune.mesh);
+  else scene.remove(this.object.bodies.neptune.mesh);
+});
+this.sceneObject.bodies.earth.controller.onChange(function(value) {
+  if (value) scene.add(this.object.bodies.earth.mesh);
+  else scene.remove(this.object.bodies.earth.mesh);
+});
+
+function SceneObject(scene) {
+  this.scene = scene;
+  this.bodies = {};
+}
+
+function PlanetInfo() {
+  this.radius = 1;
+  this.mass = 1;
+  this.speed = 10;
+  this.location = new THREE.Vector3(0, 4, -5);
+  this.inScene = false;
+  this.controller = undefined;
+  this.mesh = undefined;
+}
+
 
 // Load the background texture
 var texture = new THREE.TextureLoader().load('textures/stars_milky_way.jpg');
@@ -75,7 +139,6 @@ backgroundScene.add(backgroundMesh );
 var render = function() {
   requestAnimationFrame(render);
   earth.rotation.y += 0.01;
-  //jupiter.rotation.z += 0.015;
   controls.update();
   renderer.autoClear = false;
   renderer.clear();
